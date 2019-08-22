0e315b800aceb905e1a64c06b25f6b66c72bfb6a
sansitu <santosh_dms2003@yahoo.com>
2019-08-22 22:13:19 +0530

    Initial upload of the code

---
+++ src/Maths.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Maths;
+
+class Maths
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function sub($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function multi($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function div($a, $b)
+    {
+        return $a / $b;
+    }
+}
